51f8af5b7ccafee87ac498a4a678ea3fcd943cba
wurstbox <pet@wurstbox.com>
2021-03-23 11:13:38 +0100

    Feature 2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,17 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		sayHello();
+		runFeature2();
+	}
+	
+	private static void sayHello()
+	{
 		System.out.println("Hello World");
+	}
+	
+	private static void runFeature2()
+	{
+		System.out.println("This is feature 2");
 	}
 }
