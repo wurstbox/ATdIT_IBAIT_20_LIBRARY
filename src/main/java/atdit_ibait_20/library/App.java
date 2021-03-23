1ce4ca013fcd39dfa3077eb81e6e69f00c0259b6
wurstbox <pet@wurstbox.com>
2021-03-23 12:24:41 +0100

    Basic JFX application

---
+++ src/main/java/atdit_ibait_20/library/App.java
@@ -0,0 +1,19 @@
+package atdit_ibait_20.library;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+		stage.setTitle("Hello, World");
+		stage.show();
+	}
+}
---
+++ src/main/java/atdit_ibait_20/library/Main.java
@@ -0,0 +1,9 @@
+package atdit_ibait_20.library;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		App.main(args);
+	}
+}
